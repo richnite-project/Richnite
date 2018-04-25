971e3e1001c52314e0705359aab61e7a503d7ead
stevebrush <steevebrush@gmail.com>
2018-04-25 16:00:25 +0200

    Add new cn algorithms and use it

--- src/CryptoNoteCore/CachedBlock.cpp
+++ src/CryptoNoteCore/CachedBlock.cpp
@@ -58,21 +58,28 @@ const Crypto::Hash& CachedBlock::getBlockHash() const {
 }
 
 const Crypto::Hash& CachedBlock::getBlockLongHash(cn_context& cryptoContext) const {
-  if (!blockLongHash.is_initialized()) {
-    if (block.majorVersion == BLOCK_MAJOR_VERSION_1) {
-      const auto& rawHashingBlock = getBlockHashingBinaryArray();
-      blockLongHash = Hash();
-      cn_slow_hash(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
-    } else if (block.majorVersion >= BLOCK_MAJOR_VERSION_2) {
-      const auto& rawHashingBlock = getParentBlockHashingBinaryArray(true);
-      blockLongHash = Hash();
-      cn_slow_hash(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
-    } else {
-      throw std::runtime_error("Unknown block major version.");
+    if (!blockLongHash.is_initialized()) {
+        if (block.majorVersion == BLOCK_MAJOR_VERSION_1) {
+            const auto& rawHashingBlock = getBlockHashingBinaryArray();
+            blockLongHash = Hash();
+            cn_slow_hash_v6(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+        } else if (block.majorVersion == BLOCK_MAJOR_VERSION_2) {
+            const auto& rawHashingBlock = getParentBlockHashingBinaryArray(true);
+            blockLongHash = Hash();
+            cn_slow_hash_v6(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+        } else if (block.majorVersion == BLOCK_MAJOR_VERSION_3) {
+            const auto& rawHashingBlock = getParentBlockHashingBinaryArray(true);
+            blockLongHash = Hash();
+            cn_slow_hash_v6(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+        } else if (block.majorVersion == BLOCK_MAJOR_VERSION_4) {
+            const auto& rawHashingBlock = getParentBlockHashingBinaryArray(true);
+            blockLongHash = Hash();
+            cn_lite_slow_hash_v1(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+        } else {
+            throw std::runtime_error("Unknown block major version.");
+        }
     }
-  }
-
-  return blockLongHash.get();
+    return blockLongHash.get();
 }
 
 const Crypto::Hash& CachedBlock::getAuxiliaryBlockHeaderHash() const {
