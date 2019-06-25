eed8f5666f16e726a845fec26726eef9551a1dc0
rocky <31123976+rockstar0009@users.noreply.github.com>
2019-06-25 15:25:18 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
-// Copyright (c) 2017-2019, The Iridium developers
+// Copyright (c) 2017-2019, The Richnite developers
 // You should have received a copy of the GNU Lesser General Public License
 // If not, see <http://www.gnu.org/licenses/>.
 
